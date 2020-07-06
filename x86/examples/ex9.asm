237d75383f95dbb419e754277c97caa8e2b83322
aniket <ani1998ket@gmail.com>
2020-07-06 20:34:18 +0530

    Added example for function call with arguments

---
+++ x86/examples/ex9.asm
@@ -0,0 +1,25 @@
+; Function with arguments passed onto stack
+; Use echo $? to view the result
+
+global _start:
+
+_start:
+    push 21                 ; argument passed to times2
+    call times2             ; call()
+    mov ebx, eax            ; copying returned value from eax to ebx as exit status
+    mov eax, 1              ; sys_exit
+    int 0x80            
+
+times2:
+    push ebp                ; prologue
+    mov ebp, esp
+    
+    mov eax, [ebp + 8]      ; ebp + 8 is start of address of arguments pushed onto stack
+    add eax, eax            ; eax stores the return value of function( 2 * argument )
+    
+    mov esp, ebp            ; epilogue
+    pop ebp
+    ret
+
+
+; Remember to pop off the arguments from the stack in the caller instruction
